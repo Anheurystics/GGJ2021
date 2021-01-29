20c2cbe323a72c4ba22cc11c3a51a46bb6d80f4d
Francis dela Cruz <franciszac.dlc@gmail.com>
2021-01-29 22:21:54 +0800

    Add fade-in background for the item description UI

--- Assets/Scripts/UI/ItemDescription.cs
+++ Assets/Scripts/UI/ItemDescription.cs
@@ -7,7 +7,9 @@ using UnityEngine.EventSystems;
 
 public class ItemDescription : MonoBehaviour
 {
+    public float tweenDuration;
     private Transform itemPanel;
+    private Transform itemBackground;
     private Vector3 destPos;
     private Vector3 srcPos;
     private bool isActive;
@@ -15,7 +17,9 @@ public class ItemDescription : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        this.tweenDuration = 0.5f;
         this.itemPanel = transform.Find("ItemDescriptionPanel");
+        this.itemBackground = transform.Find("ItemDescriptionBackground");
         this.destPos = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
         this.srcPos = this.itemPanel.position;
         this.isActive = false;
@@ -26,6 +30,7 @@ public class ItemDescription : MonoBehaviour
         if (!this.isActive)
         {
             this.itemPanel.DOMoveX(this.destPos.x, 0.2f);
+            this.itemBackground.GetComponent<Image>().DOFade(0.2f, this.tweenDuration);
         }
         this.isActive = true;
     }
@@ -35,6 +40,7 @@ public class ItemDescription : MonoBehaviour
         if (this.isActive)
         {
             this.itemPanel.DOMoveX(this.srcPos.x, 0.2f);
+            this.itemBackground.GetComponent<Image>().DOFade(0.0f, this.tweenDuration);
         }
         this.isActive = false;
     }
