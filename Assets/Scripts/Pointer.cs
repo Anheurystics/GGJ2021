e3bb653a986707a6441e3a9749e63ad84042f619
Willard <willard.torres1008@gmail.com>
2021-02-01 21:34:53 +0800

    Change how pointer works; add pointer to main menu

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pointer : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Image image;
     [SerializeField] private Sprite closeSprite;
     [SerializeField] private Sprite openSprite;
     
@@ -13,13 +15,13 @@ public class Pointer : MonoBehaviour
 
     void Update()
     {
-        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pos.x += 0.5f;
-        pos.y += -0.5f;
+        var pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        pos.x += 20f;
+        pos.y += -20f;
         pos.z = 10;
 
         transform.position = pos;
 
-        sprite.sprite = (Input.GetMouseButton(0) || Input.GetMouseButton(1) || Item.currentSelected != null)? closeSprite : openSprite;
+        image.sprite = (Input.GetMouseButton(0) || Input.GetMouseButton(1) || Item.currentSelected != null)? closeSprite : openSprite;
     }
 }
